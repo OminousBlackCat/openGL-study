601545fafec73b4f5beffad4a3f0cfb52c6ecf3d
LostBlackCat <wxy1999814@gmail.com>
2022-09-13 18:22:40 +0800

    更新多光源

--- chapter2-lighting/project3-multibox/fragmentShader.fs
+++ chapter2-lighting/project3-multibox/fragmentShader.fs
@@ -1,9 +1,10 @@
 #version 330 core
 struct Material{
-    vec3 ambient;
-    vec3 diffuse;
+    //物体的材质选项
+    vec3 ambient; //物体反射环境光的颜色
+    vec3 diffuse; //物体漫反射颜色
     vec3 specular;
-    float shininess;
+    float shininess; //物体的镜面反射材质选项 越大镜面反射光圈越小(越像镜子、越金属)
 };
 
 struct Light{
@@ -11,6 +12,34 @@ struct Light{
     vec3 ambient;
     vec3 diffuse;
     vec3 specular;
+};
+
+struct DirctionalLight{
+    //平行光(定向光)
+    //无需光源位置 (光源无限远)
+    vec3 direction; //光的方向 三维向量
+    vec3 ambinet; //环境光颜色
+    vec3 diffuse; //漫反射颜色
+    vec3 specular; //镜面反射颜色;
+};
+
+struct PointLight{
+    //点光源
+    //需要光源位置 并且光会沿着距离衰减(强度)
+    vec3 position; //点光源位置
+    vec3 ambient;  //环境光颜色
+    vec3 diffuse;  //漫反射颜色
+    vec3 specular; //镜面反射颜色
+    float attenuation_constant;  //衰减常数项
+    float attenuation_linear;    //衰减线性项
+    float attenuation_quadratic; //衰减二次项
+};
+
+struct FlashLight{
+    //聚光(手电筒)
+    vec3 position; //手电筒位置
+    vec3 direction; //手电筒方向
+    float cutOff;
 };
 
 in vec2 texCoord;
